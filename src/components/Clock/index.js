ec6d033f7a96a9be67b35759ef254ec7efad08f2
Mahesh Srisailam <srisailammahesh77@gmail.com>
2022-06-21 15:37:37 +0000

    Clock

---
+++ src/components/Clock/index.js
@@ -0,0 +1,28 @@
+import {useState, useEffect} from 'react'
+import {ClockContainer, Heading, ClockImage, Time} from './styledComponents'
+
+const Clock = () => {
+  const [date, setDate] = useState(new Date())
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setDate(new Date())
+    }, 1000)
+    console.log('Effect is executed')
+    return () => {
+      clearInterval(timerId)
+      console.log('Clean up function is called')
+    }
+  }, [])
+  return (
+    <ClockContainer>
+      <Heading>Clock</Heading>
+      <ClockImage
+        src="https://assets.ccbp.in/frontend/hooks/clock-img.png"
+        alt="clock"
+      />
+      <Time> {date.toLocaleTimeString()} </Time>
+    </ClockContainer>
+  )
+}
+
+export default Clock
